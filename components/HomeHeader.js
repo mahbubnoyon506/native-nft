0ed5e8583e740e462f7add06ac1d3aa38df03826
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-03-12 20:16:13 +0600

    search bar added on header

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { assets, COLORS, SIZES } from "../constants";
+import { assets, COLORS, FONTS, SIZES } from "../constants";
 import { Image } from "react-native";
 
 const HomeHeader = () => {
@@ -9,25 +9,68 @@ const HomeHeader = () => {
       style={{
         backgroundColor: COLORS.primary,
         padding: SIZES.font,
-      }}>
+      }}
+    >
       <View
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
           alignItems: "center",
-        }}>
-        <Image 
-        source={assets.logo}
-        resizeMode='contain'
-        style={{width: 90, height: 25}}
-         />
-         <View style={{width: 45, height: 45}}>
-           <Image 
-           source={assets.person01}
-           resizeMode= 'contain'
-           style={{width: '100%', height: '100%'}}
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 90, height: 25 }}
+        />
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 15,
+              height: 15,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+        </View>
+      </View>
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}
+        >
+          Hello, Victoria
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.white,
+            marginTop: SIZES.base / 2,
+          }}
+        >
+          Let's find a masterpiece.
+        </Text>
+      </View>
+      <View style={{marginTop: SIZES.font}}>
+          <View>
+            <Image
+            source={assets.search}
+            resizeMode= 'contain'
+            style={{width: 20, height: 20, marginRight: SIZES.base}}
             />
-         </View>
+          </View>
       </View>
     </View>
   );
