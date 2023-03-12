c4078a9f16ffa934405b49081f4326b8dc1d6f5b
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-03-12 18:56:58 +0600

    sub info components all complete

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,64 +1,98 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { assets, SIZES } from '../constants'
-import { Image } from 'react-native'
+import { View, Text } from "react-native";
+import React from "react";
+import { assets, COLORS, FONTS, SHADOWS, SIZES } from "../constants";
+import { Image } from "react-native";
 
 export const NFTTitle = () => {
   return (
     <View>
       <Text>SubInfo</Text>
     </View>
-  )
-}
+  );
+};
 export const EthPrice = () => {
   return (
     <View>
       <Text>SubInfo</Text>
     </View>
-  )
-}
-export const ImageCmp = ({imgUrl, index}) => {
+  );
+};
+export const ImageCmp = ({ imgUrl, index }) => {
   return (
-    <Image 
-    source={imgUrl}
-    resizeMode='contain'
-    style={{
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
         width: 48,
         height: 48,
-        marginLeft: index === 0 ? 0 : -SIZES.font
-    }}
+        marginLeft: index === 0 ? 0 : -SIZES.font,
+      }}
     />
-  )
-}
+  );
+};
 export const People = () => {
   return (
-    <View style={{
-        flexDirection:'row'
-        }}>
-            {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) => (
-                <ImageCmp imgUrl={imgUrl} index={index} />
-            ))}
+    <View
+      style={{
+        flexDirection: "row",
+      }}
+    >
+      {[assets.person02, assets.person03, assets.person04].map(
+        (imgUrl, index) => (
+          <ImageCmp imgUrl={imgUrl} index={index} />
+        )
+      )}
     </View>
-  )
-}
+  );
+};
 export const EndDate = () => {
   return (
-    <View>
-      <Text>SubInfo</Text>
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: "center",
+        alignItems: "center",
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: "50%",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+        }}
+      >
+        Ending IN
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary,
+        }}
+      >
+        12h 30m
+      </Text>
     </View>
-  )
-}
+  );
+};
 export const SubInfo = () => {
   return (
-    <View style={{
-        width: '100%',
+    <View
+      style={{
+        width: "100%",
         paddingHorizontal: SIZES.font,
         marginTop: -SIZES.extraLarge,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    }}>
-     <People />
-     <EndDate />
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <People />
+      <EndDate />
     </View>
-  )
-}
+  );
+};
